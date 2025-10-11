da0b24a892103135d56e4d31bf1545ba0ac80c32
aaronrs2002 <aaronrs2002@yahoo.com>
2025-10-11 12:43:04 -0500

    Update functionality.js
    Cleared out idea

--- js/functionality.js
+++ js/functionality.js
@@ -892,7 +892,6 @@ function deal() {
     }
     for (let i = 0; i < 4; i++) {
         generatePlayer(i);
-                            document.querySelector("[data-player='" + i + "']").innerHTML = "";
     }
     return false;
 }
