089f9c182317a948c5fd1c696d4ae2bb44b24d0f
Aaron Smith <aaron@web-presence.biz>
2025-10-15 12:35:38 -0700

    mobile player 2 bug18

--- js/functionality.js
+++ js/functionality.js
@@ -816,15 +816,22 @@ function match(checked, betMultiplier) {
 
 function deal() {
     /*start instane mobile bug #2*/
-    document.querySelector("[data-player='1']").textContent = plyr + "Player 2";
+    if (window.innerWidth < 1200) {
 
-    [].forEach.call(document.querySelectorAll("[data-player]"), function (e) {
-        e.innerHTML = "";
-    });
+        setTimeout(() => {
+            document.querySelector("[data-player='1']").textContent = plyr + "Player 2";
 
-    for (let i = 0; i < 4; i++) {
-        document.querySelector("[data-player='" + i + "']").innerHTML = plyr + "Player " + (i + 1);
+            [].forEach.call(document.querySelectorAll("[data-player]"), function (e) {
+                e.innerHTML = "";
+            });
+
+            for (let i = 0; i < 4; i++) {
+                document.querySelector("[data-player='" + i + "']").innerHTML = plyr + "Player " + (i + 1);
+            }
+        }, 300);
+
     }
+
     /*end instane mobile bug #2*/
     for (let i = 0; i < playerIds.length; i++) {
         document.getElementById(playerIds[i]).innerHTML = ""
