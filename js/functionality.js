6b811529e7f892b3e7b6ea0698b28cb453fe535f
aaronrs2002 <aaronrs2002@yahoo.com>
2025-10-15 07:28:18 -0700

    Update functionality.js
    player two mobile not updating

--- js/functionality.js
+++ js/functionality.js
@@ -892,7 +892,7 @@ function deal() {
     }
     for (let i = 0; i < 4; i++) {
         generatePlayer(i);
-        document.querySelector("[data-player='" + i + "']").innerHTML = plyr + "Player " + (i + 1);
+       // document.querySelector("[data-player='" + i + "']").innerHTML = plyr + "Player " + (i + 1);
     }
     return false;
 }
